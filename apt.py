6679fd89af8e44ae78c05ce4fe2dea47a0c8b0d4
Athur Maciel <arthurmaciel1546@gmail.com>
2026-02-13 08:53:23 -0300

    Add environment test and Plotly import message
    Added test messages for environment and Plotly loading.

--- apt.py
+++ apt.py
@@ -1,4 +1,11 @@
 import streamlit as st
+
+st.write("Teste de ambiente")
+
+import plotly
+st.write("Plotly carregado!")
+
+import streamlit as st
 import pandas as pd
 import plotly.express as px
 
